7052ec613e63736edb899b429267b92825ddcffb
Mudoo <mudoo@qq.com>
2020-02-20 12:01:00 +0800

    update demo

--- demo/script.js
+++ demo/script.js
@@ -1,21 +1,24 @@
-import 'quill/dist/quill.snow.css';
-import '../src/assets/resize.css';
+import 'quill/dist/quill.snow.css'
+import '../src/assets/resize.css'
 
-import _Quill from 'quill';
-const Quill = window.Quill || _Quill;
+import _Quill from 'quill'
+const Quill = window.Quill || _Quill
 
-import Resize from '../src/index';
-import PlaceholderRegister, { EmbedPlaceholder } from '../src/formats/placeholder'
+import Resize, {
+    EmbedPlaceholder,
+    PlaceholderRegister,
+    convertPlaceholderHTML
+} from '../src/index'
 
-Quill.register('modules/resize', Resize);
+Quill.register('modules/resize', Resize)
 
-class TagPlaceholder extends EmbedPlaceholder { }
+class TagPlaceholder extends EmbedPlaceholder {}
 // default to ['iframe', 'video']
 TagPlaceholder.tagName = ['iframe', 'video', 'embed']
 // important!!! must be null or don't set it
 // TagPlaceholder.className = null
 
-class ClassPlaceholder extends EmbedPlaceholder { }
+class ClassPlaceholder extends EmbedPlaceholder {}
 ClassPlaceholder.className = 'ql-embed'
 
 PlaceholderRegister([TagPlaceholder, ClassPlaceholder])
@@ -39,9 +42,18 @@ const demoEditor = new Quill('#editor', {
         ],
         resize: {}
     }
-});
+})
 
-document.querySelector('.btn-save').addEventListener('click', function () {
-    const result = demoEditor.getText();
-    document.querySelector('#result').value = result
+const $result = document.querySelector('#result')
+document.querySelector('.btn-html').addEventListener('click', function () {
+    const html = convertPlaceholderHTML(demoEditor.root.innerHTML)
+    debugger
+    $result.value = html
+})
+document.querySelector('.btn-content').addEventListener('click', function () {
+    const result = demoEditor.getContents()
+    $result.value = JSON.stringify(result)
+})
+document.querySelector('.btn-text').addEventListener('click', function () {
+    $result.value = demoEditor.getText()
 })
