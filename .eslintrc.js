8957d5b1ad0f7047ca1a0bb42f62ed000fad8468
Mudoo <mudoo@qq.com>
2020-02-21 18:09:11 +0800

    eslint standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,23 @@
-// http://eslint.org/docs/user-guide/configuring
+const isDev = process.env.NODE_ENV === 'development'
 module.exports = {
-	parserOptions: {
-		sourceType: 'module'
-	},
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true,
-		node: true
-	},
-	// https://github.com/standard/standard/blob/master/docs/RULES-en.md
-	// extends: 'standard',
-	extends: 'eslint:recommended',
-	// required to lint *.vue files
-	plugins: [
-		'html'
-	],
-	"globals": {
-		"$": true,
-		"define": true,
-		"require": true
-    },
-	// add your custom rules here
-	'rules': {
-		// allow paren-less arrow functions
-		'arrow-parens': 0,
-		// allow async-await
-		'generator-star-spacing': 0,
-		// allow debugger during development
-		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-	}
+  extends: [
+    'standard',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  // add your custom rules here
+  rules: {
+    "no-console": "warn",
+    "no-debugger": isDev ? "off" : "error",
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    "linebreak-style": ["error", "unix"],
+  }
 }
